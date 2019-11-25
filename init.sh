9ebae0314fa5a97f804b0d8b81a511d2ddf693cd
Error323 <folkerthuizinga@gmail.com>
2019-11-25 11:06:30 +0100

    spi working without crc

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+STM32CUBE_DIR=$HOME/dev/stm/tools/STM32Cube_FW_F1_V1.7.0
+
+set -e
+
+case $1 in
+  Debug|Release|MinSizeRel)
+    BUILDTYPE=$1
+    shift
+    ;;
+  *)
+    BUILDTYPE=MinSizeRel
+    ;;
+esac
+
+mkdir -p build/${BUILDTYPE}
+pushd build/${BUILDTYPE}
+
+rm -rf *
+
+cmake \
+  -DSTM32CUBE=${STM32CUBE_DIR} \
+  -DCMAKE_BUILD_TYPE=${BUILDTYPE} \
+  -GNinja ../..
+
+ninja
+
+popd
