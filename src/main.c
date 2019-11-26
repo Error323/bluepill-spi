01823874be01a265e84aefffd37c308707fa6349
Error323 <folkerthuizinga@gmail.com>
2019-11-26 09:34:21 +0100

    cleanup main

--- src/main.c
+++ src/main.c
@@ -1,60 +1,5 @@
-/**
-  ******************************************************************************
-  * @file    Examples_LL/SPI/SPI_TwoBoards_FullDuplex_DMA/Src/main.c
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    14-April-2017
-  * @brief   This example describes how to send/receive bytes over SPI IP using
-  *          the STM32F1xx SPI LL API.
-  *          Peripheral initialization done using LL unitary services functions.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/** @addtogroup STM32F1xx_LL_Examples
-  * @{
-  */
-
-/** @addtogroup SPI_TwoBoards_FullDuplex_DMA
-  * @{
-  */
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Uncomment this line to use the board as master, if not it is used as slave */
-//#define MASTER_BOARD
-
-/* Private macro -------------------------------------------------------------*/
 #define CRC16_CCITT 0x1021
 #define SPI_MSG_LEN 8
 
@@ -80,12 +25,7 @@ void     LED_Init(void);
 void     LED_On(void);
 void     LED_Blinking(uint32_t Period);
 void     LED_Off(void);
-#ifdef MASTER_BOARD
-void     UserButton_Init(void);
-void     WaitForUserButtonPress(void);
-#endif
 void     WaitAndCheckEndOfTransfer(void);
-uint8_t  Buffercmp8(uint8_t* pBuffer1, uint8_t* pBuffer2, uint8_t BufferLength);
 /* Private functions ---------------------------------------------------------*/
 
 /**
